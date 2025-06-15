03f4eb5e77c90c7ab6c663fea8d1f98e915d3dd8
Nathan Ginta <nathangintat@gmail.com>
2025-06-15 18:00:44 +0700

    favicon.ico for logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
     title: 'Moodie',
     icons: {
-        icon: '/buble-chat.png',
+        icon: '/favicon.ico',
     },
 };
 
